a35ba0688c0db30c7d3f59be9089f76fd3233ef7
perception952 <952678218@qq.com>
2020-04-20 19:33:30 +0800

    1.0 can work

---
+++ src/test/java/com/imooc/client/ClientApplicationTests.java
@@ -0,0 +1,11 @@
+package com.imooc.client;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
